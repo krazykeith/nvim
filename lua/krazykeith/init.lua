c70b4dea348374d5020444a31b910cfb48b61989
Keith Lassila <keith.lassila@gosteelhead.com>
2023-12-11 07:18:40 -0500

    Sql Format

--- lua/krazykeith/init.lua
+++ lua/krazykeith/init.lua
@@ -31,3 +31,4 @@ autocmd({"BufWritePre"}, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.keymap.set('v','<C-f>', "<ESC>:'<,'>! pg_format --spaces 2 --function-case 2 --wrap-after 2 --nogrouping <CR>")
