9265ed04a57e75f9e849d8e370bbcf987ca2497f
Keith Lassila <keith.lassila@gosteelhead.com>
2024-08-20 11:26:02 -0400

    Remove old packer files

--- after/plugin/git-blame.lua
+++
@@ -1,6 +0,0 @@
-local blame = require('gitblame')
-local blame2 = require('blame')
-
-vim.keymap.set('n', '<leader>b', '<cmd>GitBlameToggle<cr> <Bar> <cmd>ToggleBlame virtual<cr>')
-vim.keymap.set('n', '<leader>o', '<cmd>GitBlameOpenCommitURL<cr>')
-blame.setup({})
--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
 require("krazykeith")
-<<<<<<< HEAD
-if vim.g.vscode then
-    -- VSCode extension
-else
-    -- ordinary Neo&ivim
-end
-=======
->>>>>>> 6d0e7ce (Update nvim config)
