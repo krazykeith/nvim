11ce45440e7ae4bfed1f2f6c18293293bf6d1b3f
Keith Lassila <keith.lassila@gosteelhead.com>
2025-07-01 14:55:10 -0400

    Adjustments

--- lua/krazykeith/lazy/init.lua
+++ lua/krazykeith/lazy/init.lua
@@ -6,4 +6,19 @@ return {
     },
 
     "eandrju/cellular-automaton.nvim",
+
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup({
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol',
+                    delay = 300,
+                    ignore_whitespace = false,
+                },
+            })
+        end
+    },
 }
